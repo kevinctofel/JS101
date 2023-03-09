c3ab0880145ab73a5a4588f00ba1b4db7dfcc91b
Kevin C. Tofel <kevin@kctofel.com>
2023-03-09 13:53:04 -0500

    refactored calculator

--- JS101/Lesson2/calculator.js
+++ JS101/Lesson2/calculator.js
@@ -4,28 +4,55 @@
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
 const readline = require('readline-sync');
+// Refactor the prompt
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
 
-console.log('Welcome to calculator!');
+prompt('Welcome to calculator!');
 
-console.log('What\'s the first number?');
+prompt('What\'s the first number?');
 let number1 = readline.question();
+// Add input validation
+while (invalidNumber(number1)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+
 console.log('What\'s the second number?');
 let number2 = readline.question();
+while (invalidNumber(number2)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number2 = readline.question();
+}
 
 console.log(`${number1} , ${number2}`);
 
 console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
 let operation = readline.question();
 
-let output;
-if (operation === '1') { // '1' represents addition
-  output = Number(number1) + Number(number2);
-}  else if (operation === '2') { // '2' represents subtraction
-  output = Number(number1) - Number(number2);
-} else if (operation === '3') { // 3 represents multiplication
-  output = Number(number1) * Number(number2);
-} else if (operation === '4') { // 4 represents division
-  output = Number(number1) / Number(number2);
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3 or 4');
+  operation = readline.question();
+}
+
+let output; // Refactor if/else to switch statement
+switch (operation) {
+  case '1' :
+    output = Number(number1) + Number(number2);
+    break;
+  case '2' :
+    output = Number(number1) - Number(number2);
+    break;
+  case '3' :
+    output = Number(number1) * Number(number2);
+    break;
+  case '4' :
+    output = Number(number1) / Number(number2);
+    break;
 }
 
 console.log(`The result is: ${output}`);
