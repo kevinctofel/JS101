b95732ecbdacd0b2fdc7853e471592c24fb109b1
Kevin C. Tofel <kevin@kctofel.com>
2023-05-08 13:15:58 -0400

    Completed Easy 2

---
+++ SmallProblems/Easy1/2.js
@@ -0,0 +1,8 @@
+// Log all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+let counter = 1;
+do {
+  console.log(counter);
+  counter += 2;
+
+} while (counter <= 99);
